bbbbaf9219b064f0d38572d08b7bf327cee9be90
ken-patcharanat <patcha-ranat@outlook.com>
2025-01-09 23:38:27 +0700

    adjust custom-values yaml

--- Makefile
+++ Makefile
@@ -1,21 +1,16 @@
-install-py-mac:
+install-py:
 	python3 -m venv pyenv
-	chmod +x ./pyvenv/bin/activate
-	source ./pyvenv/bin/activate
-
-install-py-window:
-	python3 -m venv pyenv
-	chmod +x ./pyvenv/bin/activate
-	pyvenv/bin/activate
+	chmod +x ./pyenv/bin/activate
+	# source ./pyenv/bin/activate
+	# source ./pyenv/Scripts/activate
 
 clean-py:
-	deactivate
-	rm -r pyvenv
-	
+	# deactivate
+	rm -r pyenv
 
 init-project:
 	chmod +x ./tools/setup-k8s.sh
-	chmod +x  ./tools/cleanup-k8s.sh
+	chmod +x ./tools/cleanup-k8s.sh
 
 kube-up:
 	./tools/setup-k8s.sh
--- tools/setup-k8s.sh
+++ tools/setup-k8s.sh
@@ -6,13 +6,19 @@ helm repo add apache-airflow https://airflow.apache.org
 
 helm repo update
 
-# create persistent volume claims for dags and logs
-kubectl apply -f ./k8s/airflow/persistent-volume.yaml
-kubectl apply -f ./k8s/airflow/persistent-volume-claim.yaml
-
+kubectl apply -f k8s/airflow/secrets
 
 # install airflow with helm
-helm upgrade --install airflow charts/airflow --namespace airflow -f ./k8s/airflow/custom-values.yaml
+helm upgrade \
+    --install airflow apache-airflow/airflow \
+    --version 1.15.0 \
+    --namespace airflow \
+    --values ./k8s/airflow/custom-values.yaml \
+    --timeout 10m0s \
+    --debug
+
+# create persistent volume claims for dags and logs
+kubectl apply -f ./k8s/airflow/volumes
 
 # expose port airflow webserver
 kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow
